da5b77c418977470976f35f9f8e14ed1d2316eea
Naoki Takahashi <naoki.takahashi060@gmail.com>
2025-04-27 01:18:33 +0900

    Refactor lsp.lua

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,21 +1,13 @@
 return {
   {
     'neovim/nvim-lspconfig',
-    -- version = 'v1.1.0',  -- for neovim v0.9.5
+    -- version = 'v1.1.0', -- for neovim 0.9.5
     version = 'v2.0.0',
     dependencies = {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
     },
     config = function()
-      require('mason').setup({})
-
-      local lspconfig = require 'lspconfig'
-
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-      -- NOTE: lua_ls has custom settings below, so omitted from general servers
       local servers = {
         'clangd',
         'rust_analyzer',
@@ -25,16 +17,26 @@ return {
         'jedi_language_server',
         'opencl_ls',
       }
-      local on_attach = function(client, bufnr)
-        local opts = { buffer = bufnr, noremap = true, silent = true }
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-      end
 
-      require('mason-lspconfig').setup { ensure_installed = servers }
+      require('mason').setup({})
+      require('mason-lspconfig').setup {
+        ensure_installed = servers,
+      }
+      local lspconfig = require 'lspconfig'
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
       for _, lsp in ipairs(servers) do
         lspconfig[lsp].setup {
-          on_attach = on_attach,
+          on_attach = function(_, bufnr)
+            local opts = {
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+            }
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          end,
           capabilities = capabilities,
           settings = {
             Lua = {
